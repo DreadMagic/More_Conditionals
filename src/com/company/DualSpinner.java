3e9aafb110c077bd5566538c7ef01cde9e5d9d36
Jack O'Connell <718jack@gmail.com>
2021-10-28 12:03:11 -0400

    1

---
+++ src/com/company/DualSpinner.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class DualSpinner {
+    /** Precondition: min < max
+     */
+    /*The spin method simulates a spin of a fair spinner.
+     * The method returns a random integer between min and max,
+     * inclusive. Complete the spin method below by assigning
+     * this random integer to result.
+     */
+    public int spin(int min, int max) {
+        int result = (int)(Math.random()*(max+1-min)+min);
+        return result;
+    }
+
+    /** Simulates one round of the game as described in part (b).
+     */
+    public void playRound() {
+        int player = spin(1,10);
+        int computer = spin(2,8);
+    }
+    public static void main(String[] args) {
+        DualSpinner ds = new DualSpinner();
+        for(int i = 0; i < 10; i++){
+            ds.playRound();
+        }
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -24,10 +24,8 @@ public class Main {
         else if (b < space && !(a<space)) rstate = 2;
         return rstate;
     }
-//    public static boolean makeBench(int small, int big, int goal){
-//        if (small + big*5 <= goal) return false;
-//        else
-
+    public static boolean makeBench(int small, int big, int goal){
+        return (goal - big*5 - small<=0 && goal%5 <= small);
     }
     public static void main(String[] args) {
         // leap year
@@ -37,13 +35,14 @@ public class Main {
         System.out.println("2100 " + isLeapYear(2100));
 //// is later
         System.out.println("1/2/2010 is later than 1/2/2011 " + isLater(1,2,2010, 1,2,2011));
-        System.out.println("1/2/2011 is later than 1/2/2010 " + isLater(1,2,2011, 1,2,2010));
+        System.out.println("2/1/2011 is later than 2/2/2010 " + isLater(2,1,2011, 2,2,2010));
+        System.out.println("1/2/2011 is later than 2/2/2010 " + isLater(1,2,2011, 2,2,2010));
         System.out.println("1/5/2010 is later than 3/2/2010 " + isLater(1,5,2010, 3,2,2010));
         System.out.println("3/2/2010 is later than 1/2/2010 " + isLater(3,2,2010, 1,2,2010));
         System.out.println("1/3/2010 is later than 1/2/2010 " + isLater(1,3,2010, 1,2,2010));
-        System.out.println("1/2/2010 is later than 1/3/2010 " + isLater(1,2,2010, 1,3,2010));
+        System.out.println("5/2/2010 is later than 1/3/2011 " + isLater(5,2,2010, 1,3,2011));
         System.out.println("1/2/2010 is later than 1/2/2010 " + isLater(1,2,2010, 1,2,2010));
-        System.out.println("4/8/2010 is later than 1/2/2011 " + isLater(4,8,2010, 1,2,2011));
+        System.out.println("4/8/2010 is later than 1/2/2010 " + isLater(4,8,2010, 1,2,2010));
 //// Best Fit
         System.out.println("Find Best fit 2 3 6 is " + findBestFit(2,3,6));
         System.out.println("Find Best fit 4 3 6 is " + findBestFit(4,3,6));
@@ -52,14 +51,42 @@ public class Main {
         System.out.println("Find Best fit 6 3 4 is " + findBestFit(6,3,4));
         System.out.println("Find Best fit 3 6 4 is " + findBestFit(3,6,4));
 //// Make Bench
-//        System.out.println("Make bench 3 1 8 is " + makeBench(3,1,8));
-//        System.out.println("Make bench 3 1 9 is " + makeBench(3,1,9));
-//        System.out.println("Make bench 11 1 15 is " + makeBench(11,1,15));
-//        System.out.println("Make bench 4 2 15 is " + makeBench(4,2,15));
-//        System.out.println("Make bench 20 0 20 is " + makeBench(20,0,20));
-//        System.out.println("Make bench 3 4 20 is " + makeBench(3,4,20));
-//        System.out.println("Make bench 0 6 30 is " + makeBench(0,6,30));
-//        System.out.println("Make bench 0 5 30 is " + makeBench(0,5,30));
-//        System.out.println("Make benches 2 6 23 is " + makeBenches(2,6,23));
+        System.out.println("Make bench 3 1 8 is " + makeBench(3,1,8));
+        System.out.println("Make bench 3 1 9 is " + makeBench(3,1,9));
+        System.out.println("Make bench 11 1 15 is " + makeBench(11,1,15));
+        System.out.println("Make bench 4 2 15 is " + makeBench(4,2,15));
+        System.out.println("Make bench 20 0 20 is " + makeBench(20,0,20));
+        System.out.println("Make bench 3 4 20 is " + makeBench(3,4,20));
+        System.out.println("Make bench 0 6 30 is " + makeBench(0,6,30));
+        System.out.println("Make bench 0 5 30 is " + makeBench(0,5,30));
+        System.out.println("Make bench 2 6 23 is " + makeBench(2,6,23));
     }
 }
+//2000 true
+//2004 true
+//2003 false
+//2100 false
+//1/2/2010 is later than 1/2/2011 false
+//2/1/2011 is later than 2/2/2010 true
+//1/2/2011 is later than 2/2/2010 true
+//1/5/2010 is later than 3/2/2010 false
+//3/2/2010 is later than 1/2/2010 true
+//1/3/2010 is later than 1/2/2010 true
+//5/2/2010 is later than 1/3/2011 false
+//1/2/2010 is later than 1/2/2010 false
+//4/8/2010 is later than 1/2/2010 true
+//Find Best fit 2 3 6 is 3
+//Find Best fit 4 3 6 is 1
+//Find Best fit 3 4 6 is 2
+//Find Best fit 2 3 1 is 0
+//Find Best fit 6 3 4 is 2
+//Find Best fit 3 6 4 is 1
+//Make bench 3 1 8 is true
+//Make bench 3 1 9 is false
+//Make bench 11 1 15 is true
+//Make bench 4 2 15 is false
+//Make bench 20 0 20 is true
+//Make bench 3 4 20 is true
+//Make bench 0 6 30 is true
+//Make bench 0 5 30 is false
+//Make bench 2 6 23 is false
